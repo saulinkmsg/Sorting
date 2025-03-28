13da68f1ada2e802542b9e8c87a31440e0562ddb
accjesus <accj1990@gmail.com>
2025-03-28 08:54:23 -0300

    # slide ordenação

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -5,6 +5,7 @@ public class Program
     public static void Main(string[] args)
     {
         // https://github.com/accj1990/Sorting.git
+        // https://pt.overleaf.com/read/kptbxrwtrzch#8b9776
 
         int[] vet = ManagerFileReader.Arquivo10TXT();
 
---
+++ Sorting/utils/UtilCountingTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Sorting.utils
+{
+    class UtilCountingTime
+    {
+        Stopwatch stopwatch = new Stopwatch();
+
+        public void Init()
+        {
+            stopwatch.Start();
+        }
+
+        public void Stop()
+        {
+            stopwatch.Stop();
+.
+        }
+
+
+    }
+}
